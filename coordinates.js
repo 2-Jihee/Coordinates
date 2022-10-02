d70d6444afac9addd9fc5e70a109e540bf6bb5aa
jihee <jihee0825@gmail.com>
2022-10-03 00:22:58 +0900

    add mouseFollower

--- coordinates.js
+++ coordinates.js
@@ -1,3 +1,14 @@
+const mouseFollower = document.querySelector('.mouseFollower')
+
 addEventListener('mousemove', (event) => {
-    console.log(event.clientX, event.clientY);
+    // 마우스커서 좌표값
+    const x = event.clientX
+    const y = event.clientY
+
+    // css 조작
+    mouseFollower.style.display = 'block';
+    mouseFollower.style.left = (x+20) + 'px';
+    mouseFollower.style.top = (y+5) + 'px';
+    
+    mouseFollower.innerText = `${x}px, ${y}px`
 });
